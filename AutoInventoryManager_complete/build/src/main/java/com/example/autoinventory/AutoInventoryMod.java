03ee40a8ee11d7b356ce85a6ca4dcb78dfb891ec
nhanmai001-ct1 <nhanmai030105@gmail.com>
2025-06-10 13:28:31 +0700

    Add files via upload

---
+++ AutoInventoryManager_complete/build/src/main/java/com/example/autoinventory/AutoInventoryMod.java
@@ -0,0 +1,11 @@
+package com.example.autoinventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoInventoryMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("AutoInventoryManager mod initialized!");
+        // TODO: implement full inventory logic here
+    }
+}
